4c3cea324dc4a8680c9da4ec3d717d96bc9b47e2
pogo_56 <pouya1980@gmail.com>
2026-07-13 13:12:23 +0200

    initial commit

---
+++ src/main/java/com/poonehsquest/poonehsquest/PoonehsquestApplication.java
@@ -0,0 +1,13 @@
+package com.poonehsquest.poonehsquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoonehsquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoonehsquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poonehsquest/poonehsquest/PoonehsquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poonehsquest.poonehsquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoonehsquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
