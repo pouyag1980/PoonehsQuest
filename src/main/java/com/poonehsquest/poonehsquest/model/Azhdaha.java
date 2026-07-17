9000e68edcbe2197d181a10bcabc5dc69136f7c3
pogo_56 <pouya1980@gmail.com>
2026-07-17 22:35:05 +0200

    All monster classes done

---
+++ src/main/java/com/poonehsquest/poonehsquest/model/Azhdaha.java
@@ -0,0 +1,11 @@
+package com.poonehsquest.poonehsquest.model;
+
+public class Azhdaha extends Monster{
+    public Azhdaha(){
+        monsterName = "Azhdaha";
+        numberOfMonsterAttacks = 2;
+        monsterMinDamage = 20;
+        monsterMaxDamage = 50;
+        monsterPicUrl = "/pics/azhdaha.png";
+    }
+}
---
+++ src/main/java/com/poonehsquest/poonehsquest/model/Peri.java
@@ -0,0 +1,11 @@
+package com.poonehsquest.poonehsquest.model;
+
+public class Peri extends Monster{
+    public Peri() {
+        monsterName = "Peri";
+        numberOfMonsterAttacks = 2;
+        monsterMinDamage = 0;
+        monsterMaxDamage = 10;
+        monsterPicUrl = "/pics/peri.png";
+    }
+}
