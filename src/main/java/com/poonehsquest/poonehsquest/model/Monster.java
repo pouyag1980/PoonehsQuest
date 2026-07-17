120ca557245ed46376b50a3e32c9821f08d3f96e
pogo_56 <pouya1980@gmail.com>
2026-07-17 11:14:29 +0200

    totalDamage-methos added

--- src/main/java/com/poonehsquest/poonehsquest/model/Monster.java
+++ src/main/java/com/poonehsquest/poonehsquest/model/Monster.java
@@ -3,10 +3,15 @@ package com.poonehsquest.poonehsquest.model;
 public abstract class Monster {
   String monsterName = "";
   String monsterPicUrl = "";
+  int numberOfMonsterAttacks = 0;
   int monsterMinDamage = 0;
   int monsterMaxDamage = 0;
 
   public int randomizeDamage () {
       return  monsterMinDamage +(int)(Math.random() * (monsterMaxDamage - monsterMinDamage));
   }
+  public int totalDamage() {
+    int sumOfDamage = 0; for (int attacks = 0; attacks < numberOfMonsterAttacks; attacks++)
+    { sumOfDamage = sumOfDamage + randomizeDamage(); }
+    return  sumOfDamage; }
 }
