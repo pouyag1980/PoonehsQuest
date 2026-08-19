f5c5c9f93309432ae644590c3d6514f9f4cfca2f
pogo_56 <pouya1980@gmail.com>
2026-08-19 13:41:59 +0200

    potion method added

--- src/main/java/com/poonehsquest/poonehsquest/service/BattleService.java
+++ src/main/java/com/poonehsquest/poonehsquest/service/BattleService.java
@@ -1,6 +1,7 @@
 package com.poonehsquest.poonehsquest.service;
 
 import com.poonehsquest.poonehsquest.model.Monster;
+import com.poonehsquest.poonehsquest.model.Potion;
 
 public class BattleService {
 
@@ -8,4 +9,8 @@ public class BattleService {
 public int battle ( int currentHeroHealth, Monster currentMonster ){
 return currentHeroHealth - currentMonster.totalDamage();
 }
+
+    public int usePotion(int currentHeroHealth, Potion currentPotion) {
+        return currentHeroHealth + currentPotion.calculateAddedHealthFromPotion(currentHeroHealth);
+    }
 }
