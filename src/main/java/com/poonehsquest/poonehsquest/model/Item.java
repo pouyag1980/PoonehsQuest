82e8447abcf12fc67c3f8e24fd0ee80da4fe3934
pogo_56 <pouya1980@gmail.com>
2026-08-26 13:38:41 +0200

    BattleService done

--- src/main/java/com/poonehsquest/poonehsquest/model/Item.java
+++ src/main/java/com/poonehsquest/poonehsquest/model/Item.java
@@ -1,8 +1,8 @@
 package com.poonehsquest.poonehsquest.model;
 
 public abstract class Item {
-    String itemName = "";
-    String itemPicUrl = "";
-    int addedHealth = 0;
+    public String itemName = "";
+    public String itemPicUrl = "";
+    public int addedHealth = 0;
 
 }
--- src/main/java/com/poonehsquest/poonehsquest/service/BattleService.java
+++ src/main/java/com/poonehsquest/poonehsquest/service/BattleService.java
@@ -1,7 +1,9 @@
 package com.poonehsquest.poonehsquest.service;
 
+import com.poonehsquest.poonehsquest.model.Armor;
 import com.poonehsquest.poonehsquest.model.Monster;
 import com.poonehsquest.poonehsquest.model.Potion;
+import com.poonehsquest.poonehsquest.model.Shield;
 
 public class BattleService {
 
@@ -12,5 +14,13 @@ return currentHeroHealth - currentMonster.totalDamage();
 
     public int usePotion(int currentHeroHealth, Potion currentPotion) {
         return currentHeroHealth + currentPotion.calculateAddedHealthFromPotion(currentHeroHealth);
+    }
+
+    public int useShield(int currentHeroHealth, Shield currentShield) {
+        return currentHeroHealth + currentShield.addedHealth ;
+    }
+
+    public int useArmor(int currentHeroHealth, Armor currentArmor) {
+        return currentHeroHealth + currentArmor.addedHealth ;
     }
 }
